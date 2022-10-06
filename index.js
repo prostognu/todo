7184d93f44c80d2077f9c44749c0e10988d2c18e
smysloff <kokoc.smyslov@yandex.ru>
2022-10-07 00:51:24 +0300

    wip: adding database

--- index.js
+++ index.js
@@ -12,7 +12,40 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 // database
+const sqlite3 = require('sqlite3').verbose()
+const dbname = path.join(__dirname, 'database', 'todo.db')
 
+const db = new sqlite3.Database(dbname, err => {
+  if (err) {
+    return console.error(err.message)
+  }
+  console.log("Successful connection to the database 'todo.db'")
+
+  const sql_create_books_table = `CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    comments TEXT
+  );`
+  db.run(sql_create_books_table, err => {
+    if (err) {
+      return console.error(err.message)
+    }
+    console.log("Successful creation of the 'books' table")
+  })
+
+  const sql_insert_into_books = `INSERT INTO books (id, title, author, comments) VALUES
+    (1, 'Mrs. Bridge', 'Evan S. Connell', 'First in the serie'),
+    (2, 'Mr. Bridge', 'Evan S. Connell', 'First in the serie'),
+    (3, 'L''ingenue libertine', 'Colette', 'Minne + Les egarements de Minne')
+  ;`
+  db.run(sql_insert_into_books, err => {
+    if (err) {
+      return console.error(err.message)
+    }
+    console.log("Successful creation of 3 books")
+  })
+})
 
 // routes
 app.use(express.static(path.join(__dirname, 'public'))) // static files
@@ -32,6 +65,20 @@ app.get('/data', (req, res) => {
     title: 'Данные',
     items: ['one', 'two', 'three'],
   })
+})
+app.get('/books', (req, res) => {
+
+  const sql = 'SELECT * FROM books ORDER BY title'
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return console.error(err.message)
+    }
+    res.render('books', {
+      title: 'Книги',
+      books: rows,
+    })
+  })
+
 })
 
 app.listen(3000, () => {
