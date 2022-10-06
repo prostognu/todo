e1d57a5001103173446c3e2470c45e0ee7baf647
SmyslovAG <a.smyslov@avia.mecom.ru>
2022-10-06 18:05:36 +0300

    wip: adding database

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+// Usefull links:
+// https://blog.pagesd.info/2019/10/08/crud-with-express-sqlite-10-steps/
+
+const port = 3000
 const express = require('express')
 const app = express()
+const path = require('path')
+const sqlite = require('sqlite3').verbose()
+
+// template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// database
+
+
+// routes
+app.use(express.static(path.join(__dirname, 'public'))) // static files
 
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('ProstGNU ToDo List')
-    .end()
+  res.render('index', {
+    title: 'ProstoGNU ToDo List',
+  })
+})
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'О проекте',
+  })
+})
+app.get('/data', (req, res) => {
+  res.render('data', {
+    title: 'Данные',
+    items: ['one', 'two', 'three'],
+  })
 })
 
-// Start the server
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-  console.log('Press Ctrl+C to exit.')
+app.listen(3000, () => {
+  console.log(`Server started at http://localhost:${port}`)
+  console.log('Press Ctrl+C to exit')
 })
